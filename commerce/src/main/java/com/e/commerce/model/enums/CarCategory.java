01b0d79145f755972b0c27ff71aafda4016ff0ca
herimaharavohaja <hei.haja.25@gmail.com>
2024-05-20 11:26:03 +0200

    feat: add enums CategoryCar

---
+++ commerce/src/main/java/com/e/commerce/model/enums/CarCategory.java
@@ -0,0 +1,18 @@
+package com.e.commerce.model.enums;
+
+public enum CarCategory {
+    CROSSOVER,
+    SUV,
+    VAN,
+    MINIVAN,
+    CITY_CAR,
+    SEDAN,
+    COUPE,
+    CONVERTIBLE,
+    HATCHBACK,
+    WAGON,
+    PICKUP,
+    SPORTS_CAR,
+    ELECTRIC_CAR,
+    HYBRID_CAR
+}
